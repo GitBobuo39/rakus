bca9677b4d418da3ec3158dd17e8198e3ecf9211
yuuki1039seidou@gmail.com <s1290187@u-aizu.ac.jp>
2022-07-14 17:30:38 +0900

    Hot : initial revision

---
+++ Heads.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class HeadOrTail{
+
+
+public static void main(String args[]){
+
+    Random r = new Random();
+    int x;
+    int count1=0;
+    int count2=0;
+
+    System.out.println("Tossing a coin...");
+
+    for(int i=0; i<3; i++){
+
+      x = r.nextInt(2);
+      if(x==0){
+        System.out.println("Round " + (i+1) + ": Heads");
+        count1++;
+      }else if(x==1){
+
+        System.out.println("Round " + (i+1) + ": Tails");
+        count2++;
+      }
+    }
+
+    System.out.println("Heads: " + count1 + ", Tails: " + count2);
+
+}
+
+}
