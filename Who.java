34bc59f38ad7dde4ede99872f25ec38584a77df5
yuuki1039seidou@gmail.com <s1290187@u-aizu.ac.jp>
2022-07-14 17:54:29 +0900

    User name added

---
+++ Who.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Who {
+
+
+public static void main(String args[]){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Who are you?");
+System.out.print("> ");
+String name = sc.next();
+System.out.println("Hello, " + name + "!");
+}
+
+}
